3adff68f0c39cc8f3244c175260028f7594cda0f
wallace.costa <wallace.creton@reserve.com.br>
2021-12-09 16:36:05 -0300

    Corrigindo download do aplicativo.

--- aplicativo/baixar.js
+++ aplicativo/baixar.js
@@ -1,6 +1,6 @@
 function baixar() {
     var a = document.createElement("a");
-    a.href = "aplicativo";
+    a.href = window.location.href + "/aplicativo/";
     a.setAttribute("download", "wssistemas-v1.apk");
     a.click();
 }
