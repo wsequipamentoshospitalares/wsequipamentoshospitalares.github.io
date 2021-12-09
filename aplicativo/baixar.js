82f0c87f799293a1771bf8bee4faa19ca443cdd5
wallace.costa <wallace.creton@reserve.com.br>
2021-12-09 17:13:52 -0300

    Retirando aplicativo do projeto a pedido do github e realizando o download apartir de um release definido.

--- aplicativo/baixar.js
+++
@@ -1,6 +0,0 @@
-function baixar() {
-    var a = document.createElement("a");
-    a.href = window.location.href + "/aplicativo/";
-    a.setAttribute("download", "wssistemas-v1.apk");
-    a.click();
-}
---
+++ assets/js/baixar.js
@@ -0,0 +1,5 @@
+function baixar() {
+    var a = document.createElement("a");
+    a.href = "https://github.com/wsequipamentoshospitalares/wsequipamentoshospitalares.github.io/releases/download/v1.0.0/wssistemas.apk";
+    a.click();
+}
