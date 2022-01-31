7c8128935cbbf39304ff910679cbbac929ed13af
Wallace Costa <wallacedacosta@gmail.com>
2022-01-31 10:57:46 -0300

    Permitir baixar o aplicativo por versão.

--- assets/js/baixar.js
+++ assets/js/baixar.js
@@ -1,5 +1,20 @@
-function baixar() {
+function baixar(versao) {
+    var version = "";
+
+    switch (versao) {
+        case 1:
+            version = "v1.0.0";
+            break;
+        case 2:
+            version = "v2.0.0";
+            break;
+        default:
+            version = "v1.0.0"
+            break;
+    }
+
+
     var a = document.createElement("a");
-    a.href = "https://github.com/wsequipamentoshospitalares/wsequipamentoshospitalares.github.io/releases/download/v1.0.0/wssistemas.apk";
+    a.href = "https://github.com/wsequipamentoshospitalares/wsequipamentoshospitalares.github.io/releases/download/" + version + "/wssistemas.apk";
     a.click();
 }
